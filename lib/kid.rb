ec809e99bc4e1b3a29a12743c4e4d907dbc042de
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-04-10 22:37:15 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,26 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+class Kid
+  include Dance
+  extend MetaDancing
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+end
+
+# require_relative './dance_module.rb'
+# require_relative './class_methods_module.rb'
+
+# class Kid
+  
+#   include Dance 
+  
+#   attr_accessor :name 
+  
+#   def initialize(name) 
+#     @name = name 
+#   end 
+  
+#   extend MetaDancing
+# end 
